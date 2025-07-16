82b808983d502a99edbff5d53a759b67db4350d6
BanditTech <thebbandit@gmail.com>
2025-07-16 15:51:56 -0700

    2.0.5

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,18 +1,31 @@
-local beacon_divisor = settings.startup["beacon_power_usage_divisor"].value
-local mining_divisor = settings.startup["mining_drill_resource_drain_divisor"].value
-local unlink = settings.startup["unlink_quality_from_speed"].value
+local function setting(name)
+	return settings.startup[name].value
+end
 
-local function round(value)
-	return math.max(1, math.floor(value + 0.5))
+local beacon_divisor = setting("beacon_power_usage_divisor")
+local mining_divisor = setting("mining_drill_resource_drain_divisor")
+local crafting_divisor = setting("crafting_machine_energy_usage_divisor")
+local unlink = setting("unlink_quality_from_speed")
+local max_level = math.max(setting("normal_level"), setting("uncommon_level"), setting("rare_level"),
+                           setting("epic_level"), setting("legendary_level"))
+
+local function clamp(min, max, value)
+	if value > max then
+		return max
+	elseif value < min then
+		return min
+	else
+		return value
+	end
 end
 
 local quality_mapping = {
-	[0.1] = settings.startup["quality_module_1"].value,
-	[0.2] = settings.startup["quality_module_2"].value,
-	[0.25] = settings.startup["quality_module_3"].value,
-	[-0.1] = settings.startup["speed_module_1"].value,
-	[-0.15] = settings.startup["speed_module_2"].value,
-	[-0.25] = settings.startup["speed_module_3"].value
+	[0.1] = setting("quality_module_1"),
+	[0.2] = setting("quality_module_2"),
+	[0.25] = setting("quality_module_3"),
+	[-0.1] = setting("speed_module_1"),
+	[-0.15] = setting("speed_module_2"),
+	[-0.25] = setting("speed_module_3")
 }
 
 local function modifyModule(mod)
@@ -41,81 +54,122 @@ for _, mod in pairs(data.raw.module) do
 	modifyModule(mod)
 end
 
-local normal = data.raw.quality.normal
-normal.level = settings.startup["normal_level"].value
-normal.next_probability = settings.startup["normal_next_probability"].value
+for i, obj in pairs(data.raw["quality"]) do
+	if i == "normal" then
+		obj.level = setting("normal_level")
+		obj.next_probability = setting("normal_next_probability")
+		if obj.level > 0 then
+			obj.beacon_power_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / beacon_divisor)
+			obj.mining_drill_resource_drain_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / mining_divisor)
+			obj.crafting_machine_energy_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / crafting_divisor)
+		end
+	elseif i == "uncommon" then
+		obj.level = setting("uncommon_level")
+		obj.beacon_power_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / beacon_divisor)
+		obj.mining_drill_resource_drain_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / mining_divisor)
+		obj.crafting_machine_energy_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / crafting_divisor)
+		obj.science_pack_drain_multiplier = setting("uncommon_science_pack_drain") / 100
+		obj.next_probability = setting("uncommon_next_probability")
+	elseif i == "rare" then
+		obj.level = setting("rare_level")
+		obj.beacon_power_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / beacon_divisor)
+		obj.mining_drill_resource_drain_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / mining_divisor)
+		obj.crafting_machine_energy_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / crafting_divisor)
+		obj.science_pack_drain_multiplier = setting("rare_science_pack_drain") / 100
+		obj.next_probability = setting("rare_next_probability")
+	elseif i == "epic" then
+		obj.level = setting("epic_level")
+		obj.beacon_power_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / beacon_divisor)
+		obj.mining_drill_resource_drain_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / mining_divisor)
+		obj.crafting_machine_energy_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / crafting_divisor)
+		obj.science_pack_drain_multiplier = setting("epic_science_pack_drain") / 100
+		obj.next_probability = setting("epic_next_probability")
+	elseif i == "legendary" then
+		obj.level = setting("legendary_level")
+		obj.beacon_power_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / beacon_divisor)
+		obj.mining_drill_resource_drain_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / mining_divisor)
+		obj.crafting_machine_energy_usage_multiplier = clamp(0, 1, (max_level + 1 - obj.level) / crafting_divisor)
+		obj.science_pack_drain_multiplier = setting("legendary_science_pack_drain") / 100
+	end
+	obj.default_multiplier = 1 + setting("default_multiplier") * obj.level
+	obj.tool_durability_multiplier = 1 + setting("tool_durability_multiplier") * obj.level
+	obj.beacon_supply_area_distance_bonus = setting("beacon_supply_area_distance_bonus") * obj.level
+	obj.beacon_module_slots_bonus = setting("beacon_module_slots_bonus") * obj.level
+	obj.lab_module_slots_bonus = setting("lab_module_slots_bonus") * obj.level
+	obj.crafting_machine_module_slots_bonus = setting("crafting_machine_module_slots_bonus") * obj.level
+	obj.mining_drill_module_slots_bonus = setting("mining_drill_module_slots_bonus") * obj.level
+	obj.mining_drill_mining_radius_bonus = setting("mining_drill_mining_radius_bonus") * obj.level
+	obj.equipment_grid_width_bonus = setting("equipment_grid_width_bonus") * obj.level
+	obj.equipment_grid_height_bonus = setting("equipment_grid_height_bonus") * obj.level
+	obj.asteroid_collector_collection_radius_bonus = setting("asteroid_collector_collection_radius_bonus") * obj.level
+	obj.logistic_cell_charging_station_count_bonus = setting("logistic_cell_charging_station_count_bonus") * obj.level
+	obj.flying_robot_max_energy_multiplier = 1 + setting("flying_robot_max_energy_multiplier") * obj.level
+	obj.accumulator_capacity_multiplier = 1 + setting("accumulator_capacity_multiplier") * obj.level
+	obj.electric_pole_wire_reach_bonus = setting("electric_pole_wire_reach_bonus") * obj.level
+	obj.electric_pole_supply_area_distance_bonus = setting("electric_pole_supply_area_distance_bonus") * obj.level
+	obj.range_multiplier = clamp(1, 3, 1 + setting("range_multiplier") * obj.level)
 
-local uncommon = data.raw.quality.uncommon
-uncommon.level = settings.startup["uncommon_level"].value
-uncommon.beacon_power_usage_multiplier = round((5 / 6) * beacon_divisor) / beacon_divisor
-uncommon.mining_drill_resource_drain_multiplier = round((5 / 6) * mining_divisor) / mining_divisor
-uncommon.science_pack_drain_multiplier = settings.startup["uncommon_science_pack_drain"].value / 100
-uncommon.next_probability = settings.startup["uncommon_next_probability"].value
-
-local rare = data.raw.quality.rare
-rare.level = settings.startup["rare_level"].value
-rare.beacon_power_usage_multiplier = round((4 / 6) * beacon_divisor) / beacon_divisor
-rare.mining_drill_resource_drain_multiplier = round((4 / 6) * mining_divisor) / mining_divisor
-rare.science_pack_drain_multiplier = settings.startup["rare_science_pack_drain"].value / 100
-rare.next_probability = settings.startup["rare_next_probability"].value
-
-local epic = data.raw.quality.epic
-epic.level = settings.startup["epic_level"].value
-epic.beacon_power_usage_multiplier = round((3 / 6) * beacon_divisor) / beacon_divisor
-epic.mining_drill_resource_drain_multiplier = round((3 / 6) * mining_divisor) / mining_divisor
-epic.science_pack_drain_multiplier = settings.startup["epic_science_pack_drain"].value / 100
-epic.next_probability = settings.startup["epic_next_probability"].value
-
-local legendary = data.raw.quality.legendary
-legendary.level = settings.startup["legendary_level"].value
-legendary.beacon_power_usage_multiplier = 1 / beacon_divisor
-legendary.mining_drill_resource_drain_multiplier = 1 / mining_divisor
-legendary.science_pack_drain_multiplier = settings.startup["legendary_science_pack_drain"].value / 100
+	obj.crafting_machine_speed_multiplier = setting("crafting_machine_speed_multiplier") * obj.default_multiplier
+	obj.lab_research_speed_multiplier = setting("lab_research_speed_multiplier") * obj.default_multiplier
+	obj.inventory_size_multiplier = setting("inventory_size_multiplier") * obj.default_multiplier
+	obj.fluid_wagon_capacity_multiplier = setting("fluid_wagon_capacity_multiplier") * obj.default_multiplier
+	obj.inserter_speed_multiplier = setting("inserter_speed_multiplier") * obj.default_multiplier
+	obj.logistic_cell_charging_energy_multiplier = setting("logistic_cell_charging_energy_multiplier")
+		                                               * obj.default_multiplier
+end
 
 for _, obj in pairs(data.raw["lab"]) do
-	obj.uses_quality_drain_modifier = settings.startup["lab_uses_quality_drain_modifier"].value
+	obj.uses_quality_drain_modifier = setting("lab_uses_quality_drain_modifier")
+	obj.quality_affects_module_slots = setting("quality_affects_module_slots_lab")
 end
 
 for _, obj in pairs(data.raw["cargo-wagon"]) do
-	obj.quality_affects_inventory_size = settings.startup["quality_affects_inventory_size_cargo_wagon"].value
+	obj.quality_affects_inventory_size = setting("quality_affects_inventory_size_cargo_wagon")
 end
 
 for _, obj in pairs(data.raw["container"]) do
-	obj.quality_affects_inventory_size = settings.startup["quality_affects_inventory_size_container"].value
+	obj.quality_affects_inventory_size = setting("quality_affects_inventory_size_container")
 end
 
 for _, obj in pairs(data.raw["fluid-wagon"]) do
-	obj.quality_affects_capacity = settings.startup["quality_affects_capacity_fluid_wagon"].value
+	obj.quality_affects_capacity = setting("quality_affects_capacity_fluid_wagon")
 end
 
 for _, obj in pairs(data.raw["roboport-equipment"]) do
-	obj.charging_station_count_affected_by_quality =
-		settings.startup["charging_station_count_affected_by_quality_roboport_equipment"].value
+	obj.charging_station_count_affected_by_quality = setting("charging_station_count_affected_by_quality_equipment")
 end
 
 for _, obj in pairs(data.raw["roboport"]) do
-	obj.charging_station_count_affected_by_quality =
-		settings.startup["charging_station_count_affected_by_quality_roboport"].value
+	obj.charging_station_count_affected_by_quality = setting("charging_station_count_affected_by_quality")
 end
 
 for _, obj in pairs(data.raw["chain-active-trigger"]) do
-	obj.fork_chance_increase_per_quality_level =
-		settings.startup["fork_chance_increase_per_quality_level_chain_active_trigger"].value
+	obj.fork_chance_increase_per_quality_level = setting("fork_chance_increase_per_quality_level_chain_active_trigger")
 end
 
 for _, obj in pairs(data.raw["beacon"]) do
-	obj.distribution_effectivity_bonus_per_quality_level =
-		settings.startup["distribution_effectivity_bonus_per_quality_level_beacon"].value
+	obj.distribution_effectivity_bonus_per_quality_level = setting("distribution_effectivity_bonus_per_quality_level")
+	obj.quality_affects_module_slots = setting("quality_affects_module_slots_beacon")
+	obj.quality_affects_supply_area_distance = setting("quality_affects_supply_area_distance_beacon")
 end
 
 for _, obj in pairs(data.raw["asteroid-collector"]) do
-	obj.arm_inventory_size_quality_increase =
-		settings.startup["arm_inventory_size_quality_increase_asteroid_collector"].value
-	obj.inventory_size_quality_increase = settings.startup["inventory_size_quality_increase_asteroid_collector"].value
-	obj.energy_usage_quality_scaling = settings.startup["energy_usage_quality_scaling_asteroid_collector"].value
-	obj.arm_count_quality_scaling = settings.startup["arm_count_quality_scaling_asteroid_collector"].value
-	obj.arm_speed_quality_scaling = settings.startup["arm_speed_quality_scaling_asteroid_collector"].value
-	obj.arm_angular_speed_cap_quality_scaling =
-		settings.startup["arm_angular_speed_cap_quality_scaling_asteroid_collector"].value
+	obj.arm_inventory_size_quality_increase = setting("arm_inventory_size_quality_increase_collector")
+	obj.inventory_size_quality_increase = setting("inventory_size_quality_increase_collector")
+	obj.energy_usage_quality_scaling = setting("energy_usage_quality_scaling_collector")
+	obj.arm_count_quality_scaling = setting("arm_count_quality_scaling_collector")
+	obj.arm_speed_quality_scaling = setting("arm_speed_quality_scaling_collector")
+	obj.arm_angular_speed_cap_quality_scaling = setting("arm_angular_speed_cap_quality_scaling_collector")
 end
 
+for _, obj in pairs(data.raw["mining-drill"]) do
+	obj.quality_affects_mining_radius = setting("quality_affects_mining_radius")
+	obj.quality_affects_module_slots = setting("quality_affects_module_slots_mining")
+end
+
+for _, obj in pairs(data.raw["assembling-machine"]) do
+	obj.quality_affects_energy_usage = setting("quality_affects_energy_usage_crafting")
+	if #obj.allowed_effects > 0 and obj.module_slots ~= nil and obj.module_slots > 0 then
+		obj.quality_affects_module_slots = setting("quality_affects_module_slots_crafting")
+	end
+end
--- settings.lua
+++ settings.lua
@@ -2,69 +2,29 @@
 -- settings.lua
 -- Allows users to modify the quality levels via mod settings
 data:extend({{
+	type = "double-setting",
+	name = "default_multiplier",
+	setting_type = "startup",
+	default_value = 0.3,
+	minimum_value = 0.01,
+	maximum_value = 10,
+	order = "a0"
+}, {
+	type = "double-setting",
+	name = "tool_durability_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.01,
+	maximum_value = 10,
+	order = "a1"
+}, {
 	type = "int-setting",
 	name = "normal_level",
 	setting_type = "startup",
 	default_value = 0,
 	minimum_value = 0,
 	maximum_value = 20,
-	order = "a1"
-}, {
-	type = "int-setting",
-	name = "uncommon_level",
-	setting_type = "startup",
-	default_value = 1,
-	minimum_value = 1,
-	maximum_value = 20,
-	order = "b1"
-}, {
-	type = "int-setting",
-	name = "rare_level",
-	setting_type = "startup",
-	default_value = 2,
-	minimum_value = 1,
-	maximum_value = 20,
-	order = "c1"
-}, {
-	type = "int-setting",
-	name = "epic_level",
-	setting_type = "startup",
-	default_value = 3,
-	minimum_value = 1,
-	maximum_value = 20,
-	order = "d1"
-}, {
-	type = "int-setting",
-	name = "legendary_level",
-	setting_type = "startup",
-	default_value = 5,
-	minimum_value = 1,
-	maximum_value = 20,
-	order = "e1"
-}, {
-	type = "int-setting",
-	name = "beacon_power_usage_divisor",
-	setting_type = "startup",
-	default_value = 6,
-	minimum_value = 1,
-	maximum_value = 12,
-	order = "f1"
-}, {
-	type = "double-setting",
-	name = "distribution_effectivity_bonus_per_quality_level_beacon",
-	setting_type = "startup",
-	default_value = 0,
-	minimum_value = 0,
-	maximum_value = 1,
-	order = "f1a"
-}, {
-	type = "int-setting",
-	name = "mining_drill_resource_drain_divisor",
-	setting_type = "startup",
-	default_value = 6,
-	minimum_value = 1,
-	maximum_value = 12,
-	order = "f2"
+	order = "b0"
 }, {
 	type = "double-setting",
 	name = "normal_next_probability",
@@ -72,7 +32,15 @@ data:extend({{
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "a5"
+	order = "b1"
+}, {
+	type = "int-setting",
+	name = "uncommon_level",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 1,
+	maximum_value = 20,
+	order = "b2"
 }, {
 	type = "double-setting",
 	name = "uncommon_next_probability",
@@ -80,7 +48,15 @@ data:extend({{
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "b5"
+	order = "b3"
+}, {
+	type = "int-setting",
+	name = "rare_level",
+	setting_type = "startup",
+	default_value = 2,
+	minimum_value = 1,
+	maximum_value = 20,
+	order = "b4"
 }, {
 	type = "double-setting",
 	name = "rare_next_probability",
@@ -88,7 +64,15 @@ data:extend({{
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "c5"
+	order = "b5"
+}, {
+	type = "int-setting",
+	name = "epic_level",
+	setting_type = "startup",
+	default_value = 3,
+	minimum_value = 1,
+	maximum_value = 20,
+	order = "b6"
 }, {
 	type = "double-setting",
 	name = "epic_next_probability",
@@ -96,7 +80,15 @@ data:extend({{
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "d5"
+	order = "b7"
+}, {
+	type = "int-setting",
+	name = "legendary_level",
+	setting_type = "startup",
+	default_value = 5,
+	minimum_value = 1,
+	maximum_value = 20,
+	order = "b8"
 }, {
 	type = "double-setting",
 	name = "quality_module_1",
@@ -104,7 +96,7 @@ data:extend({{
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "g1"
+	order = "c0"
 }, {
 	type = "double-setting",
 	name = "quality_module_2",
@@ -112,7 +104,7 @@ data:extend({{
 	default_value = 0.2,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "g2"
+	order = "c1"
 }, {
 	type = "double-setting",
 	name = "quality_module_3",
@@ -120,7 +112,7 @@ data:extend({{
 	default_value = 0.25,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "g3"
+	order = "c2"
 }, {
 	type = "double-setting",
 	name = "speed_module_1",
@@ -128,7 +120,7 @@ data:extend({{
 	default_value = -0.1,
 	minimum_value = -1,
 	maximum_value = 0,
-	order = "g4"
+	order = "c3"
 }, {
 	type = "double-setting",
 	name = "speed_module_2",
@@ -136,7 +128,7 @@ data:extend({{
 	default_value = -0.15,
 	minimum_value = -1,
 	maximum_value = 0,
-	order = "g5"
+	order = "c4"
 }, {
 	type = "double-setting",
 	name = "speed_module_3",
@@ -144,19 +136,167 @@ data:extend({{
 	default_value = -0.25,
 	minimum_value = -1,
 	maximum_value = 0,
-	order = "g6"
+	order = "c5"
 }, {
 	type = "bool-setting",
 	name = "unlink_quality_from_speed",
 	setting_type = "startup",
 	default_value = false,
-	order = "g7"
+	order = "c6"
+}, {
+	type = "bool-setting",
+	name = "quality_affects_energy_usage_crafting",
+	setting_type = "startup",
+	default_value = false,
+	order = "d0"
+}, {
+	type = "double-setting",
+	name = "crafting_machine_energy_usage_divisor",
+	setting_type = "startup",
+	default_value = 6,
+	minimum_value = 1,
+	maximum_value = 100,
+	order = "d1"
+}, {
+	type = "bool-setting",
+	name = "quality_affects_module_slots_crafting",
+	setting_type = "startup",
+	default_value = false,
+	order = "d2"
+}, {
+	type = "double-setting",
+	name = "crafting_machine_module_slots_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "d3"
+}, {
+	type = "double-setting",
+	name = "crafting_machine_speed_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "d4"
+}, {
+	type = "double-setting",
+	name = "inserter_speed_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "e0"
+}, {
+	type = "double-setting",
+	name = "accumulator_capacity_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "f0"
+}, {
+	type = "double-setting",
+	name = "electric_pole_wire_reach_bonus",
+	setting_type = "startup",
+	default_value = 2,
+	minimum_value = 0.1,
+	maximum_value = 20,
+	order = "f1"
+}, {
+	type = "double-setting",
+	name = "electric_pole_supply_area_distance_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "f2"
+}, {
+	type = "bool-setting",
+	name = "quality_affects_module_slots_beacon",
+	setting_type = "startup",
+	default_value = false,
+	order = "g0"
+}, {
+	type = "double-setting",
+	name = "beacon_module_slots_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "g1"
+}, {
+	type = "bool-setting",
+	name = "quality_affects_supply_area_distance_beacon",
+	setting_type = "startup",
+	default_value = false,
+	order = "g2"
+}, {
+	type = "double-setting",
+	name = "beacon_supply_area_distance_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "g3"
+}, {
+	type = "double-setting",
+	name = "distribution_effectivity_bonus_per_quality_level",
+	setting_type = "startup",
+	default_value = 0,
+	minimum_value = 0,
+	maximum_value = 1,
+	order = "g4"
+}, {
+	type = "int-setting",
+	name = "beacon_power_usage_divisor",
+	setting_type = "startup",
+	default_value = 6,
+	minimum_value = 1,
+	maximum_value = 100,
+	order = "g5"
+}, {
+	type = "int-setting",
+	name = "mining_drill_resource_drain_divisor",
+	setting_type = "startup",
+	default_value = 6,
+	minimum_value = 1,
+	maximum_value = 100,
+	order = "h0"
+}, {
+	type = "bool-setting",
+	name = "quality_affects_mining_radius",
+	setting_type = "startup",
+	default_value = false,
+	order = "h1"
+}, {
+	type = "double-setting",
+	name = "mining_drill_mining_radius_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "h2"
+}, {
+	type = "bool-setting",
+	name = "quality_affects_module_slots_mining",
+	setting_type = "startup",
+	default_value = false,
+	order = "h3"
+}, {
+	type = "double-setting",
+	name = "mining_drill_module_slots_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "h4"
 }, {
 	type = "bool-setting",
 	name = "lab_uses_quality_drain_modifier",
 	setting_type = "startup",
 	default_value = false,
-	order = "h1"
+	order = "j0"
 }, {
 	type = "int-setting",
 	name = "uncommon_science_pack_drain",
@@ -164,7 +304,7 @@ data:extend({{
 	default_value = 99,
 	minimum_value = 1,
 	maximum_value = 100,
-	order = "h2"
+	order = "j1"
 }, {
 	type = "int-setting",
 	name = "rare_science_pack_drain",
@@ -172,7 +312,7 @@ data:extend({{
 	default_value = 98,
 	minimum_value = 1,
 	maximum_value = 100,
-	order = "h3"
+	order = "j2"
 }, {
 	type = "int-setting",
 	name = "epic_science_pack_drain",
@@ -180,7 +320,7 @@ data:extend({{
 	default_value = 97,
 	minimum_value = 1,
 	maximum_value = 100,
-	order = "h4"
+	order = "j3"
 }, {
 	type = "int-setting",
 	name = "legendary_science_pack_drain",
@@ -188,37 +328,115 @@ data:extend({{
 	default_value = 95,
 	minimum_value = 1,
 	maximum_value = 100,
-	order = "h5"
+	order = "j4"
 }, {
 	type = "bool-setting",
-	name = "quality_affects_inventory_size_cargo_wagon",
+	name = "quality_affects_module_slots_lab",
 	setting_type = "startup",
 	default_value = false,
-	order = "i1"
+	order = "j5"
+}, {
+	type = "double-setting",
+	name = "lab_module_slots_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "j6"
+}, {
+	type = "double-setting",
+	name = "lab_research_speed_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "j7"
 }, {
 	type = "bool-setting",
 	name = "quality_affects_inventory_size_container",
 	setting_type = "startup",
 	default_value = true,
-	order = "i2"
+	order = "k0"
+}, {
+	type = "bool-setting",
+	name = "quality_affects_inventory_size_cargo_wagon",
+	setting_type = "startup",
+	default_value = false,
+	order = "k1"
+}, {
+	type = "double-setting",
+	name = "inventory_size_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "k2"
 }, {
 	type = "bool-setting",
 	name = "quality_affects_capacity_fluid_wagon",
 	setting_type = "startup",
 	default_value = false,
-	order = "i3"
+	order = "k3"
+}, {
+	type = "double-setting",
+	name = "fluid_wagon_capacity_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "k4"
+}, {
+	type = "double-setting",
+	name = "equipment_grid_width_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "k5"
+}, {
+	type = "double-setting",
+	name = "equipment_grid_height_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "k6"
 }, {
 	type = "bool-setting",
-	name = "charging_station_count_affected_by_quality_roboport_equipment",
+	name = "charging_station_count_affected_by_quality_equipment",
 	setting_type = "startup",
 	default_value = false,
-	order = "i4"
+	order = "l0"
 }, {
 	type = "bool-setting",
-	name = "charging_station_count_affected_by_quality_roboport",
+	name = "charging_station_count_affected_by_quality",
 	setting_type = "startup",
 	default_value = false,
-	order = "i5"
+	order = "l1"
+}, {
+	type = "double-setting",
+	name = "logistic_cell_charging_energy_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "l2"
+}, {
+	type = "double-setting",
+	name = "logistic_cell_charging_station_count_bonus",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "l2"
+}, {
+	type = "double-setting",
+	name = "flying_robot_max_energy_multiplier",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0.1,
+	maximum_value = 10,
+	order = "l3"
 }, {
 	type = "double-setting",
 	name = "fork_chance_increase_per_quality_level_chain_active_trigger",
@@ -226,53 +444,69 @@ data:extend({{
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "j1"
-}, {
-	type = "int-setting",
-	name = "arm_inventory_size_quality_increase_asteroid_collector",
-	setting_type = "startup",
-	default_value = 0,
-	minimum_value = 0,
-	maximum_value = 10,
-	order = "j3"
-}, {
-	type = "int-setting",
-	name = "inventory_size_quality_increase_asteroid_collector",
-	setting_type = "startup",
-	default_value = 5,
-	minimum_value = 0,
-	maximum_value = 20,
-	order = "j4"
+	order = "m0"
 }, {
 	type = "double-setting",
-	name = "energy_usage_quality_scaling_asteroid_collector",
+	name = "range_multiplier",
 	setting_type = "startup",
 	default_value = 0.1,
-	minimum_value = 0,
-	maximum_value = 1,
-	order = "j5"
+	minimum_value = 0.01,
+	maximum_value = 2,
+	order = "m1"
 }, {
 	type = "int-setting",
-	name = "arm_count_quality_scaling_asteroid_collector",
+	name = "asteroid_collector_collection_radius_bonus",
 	setting_type = "startup",
 	default_value = 1,
 	minimum_value = 0,
 	maximum_value = 10,
-	order = "j6"
+	order = "n0"
+}, {
+	type = "int-setting",
+	name = "inventory_size_quality_increase_collector",
+	setting_type = "startup",
+	default_value = 5,
+	minimum_value = 0,
+	maximum_value = 20,
+	order = "n1"
+}, {
+	type = "int-setting",
+	name = "arm_inventory_size_quality_increase_collector",
+	setting_type = "startup",
+	default_value = 0,
+	minimum_value = 0,
+	maximum_value = 10,
+	order = "n2"
 }, {
 	type = "double-setting",
-	name = "arm_speed_quality_scaling_asteroid_collector",
+	name = "energy_usage_quality_scaling_collector",
 	setting_type = "startup",
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "j7"
+	order = "n3"
+}, {
+	type = "int-setting",
+	name = "arm_count_quality_scaling_collector",
+	setting_type = "startup",
+	default_value = 1,
+	minimum_value = 0,
+	maximum_value = 10,
+	order = "n4"
 }, {
 	type = "double-setting",
-	name = "arm_angular_speed_cap_quality_scaling_asteroid_collector",
+	name = "arm_speed_quality_scaling_collector",
 	setting_type = "startup",
 	default_value = 0.1,
 	minimum_value = 0,
 	maximum_value = 1,
-	order = "j8"
+	order = "n5"
+}, {
+	type = "double-setting",
+	name = "arm_angular_speed_cap_quality_scaling_collector",
+	setting_type = "startup",
+	default_value = 0.1,
+	minimum_value = 0,
+	maximum_value = 1,
+	order = "n6"
 }})
