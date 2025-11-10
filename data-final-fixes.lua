b1da72a49fe3c3c9f89049a7bd61891260a9e985
BanditTech <thebbandit@gmail.com>
2025-11-09 17:24:39 -0800

    Fix for "God Module" style addons when unlink speed from quality is enabled, we ensure the modifications only happen to modules with negative values. (negative speed or quality) Increment to v2.0.61

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -23,14 +23,14 @@ local unlink = setting("unlink_quality_from_speed")
 local function modifyModule(mod)
 	local effects = mod.effect
 	if effects.speed and effects.speed > 0 then
-		if unlink then
+		if unlink and effects.quality < 0 then
 			effects.quality = 0
-		else
-			if quality_mapping[effects.quality] then effects.quality = quality_mapping[effects.quality] end
+		elseif quality_mapping[effects.quality] then
+			effects.quality = quality_mapping[effects.quality] end
 		end
 	end
 	if effects.quality and effects.quality > 0 then
-		if unlink then effects.speed = 0 end
+		if unlink and effects.speed < 0 then effects.speed = 0 end
 		if quality_mapping[effects.quality] then effects.quality = quality_mapping[effects.quality] end
 	end
 	mod.effect = effects
